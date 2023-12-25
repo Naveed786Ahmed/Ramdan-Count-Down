cde819ea4790f12ac916a9f7bef81514b8d31c41
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2023-12-25 16:11:45 +0500

    Ramdan Count Down

---
+++ app.js
@@ -0,0 +1,8 @@
+let ramdanDate = new Date("03-10-2024")
+let todayDate = new Date(prompt("Enter Your Date in MM/ DD / YYYY Formate"))
+
+let diff = ramdanDate.getTime() - todayDate.getTime()
+
+let months = diff / (1000 * 60 * 60 * 24)
+
+alert(`There are ${Math.round(months)} days left in Ramdan`)
